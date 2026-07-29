d1b30e7186194a8a3d8852aa6fc555ac1ffe763a
vv506 <thuyvy151006@gmail.com>
2026-07-29 14:06:23 +0700

    feat: set application title and favicon

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,15 @@ import { AuthProvider } from './context/AuthContext.tsx';
 import App from './App.tsx';
 import './index.css';
 
+document.title = "Nội thất gỗ Huy Hào";
+
+// Dynamically set favicon to ensure it bypasses any caching
+const link = document.querySelector("link[rel*='icon']") as HTMLLinkElement || document.createElement('link');
+link.type = 'image/jpeg';
+link.rel = 'icon';
+link.href = '/img/logo.jpg';
+document.getElementsByTagName('head')[0].appendChild(link);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
