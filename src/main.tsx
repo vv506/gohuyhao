7a559c7839c9957b06c017b4d527cea33a969fe2
vv506 <thuyvy151006@gmail.com>
2026-07-29 14:11:52 +0700

    fix: update favicon cache busting strategy
    Replace the static favicon with a PNG file and implement a dynamic versioning strategy to ensure browser caches are bypassed during updates.

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ document.title = "Nội thất gỗ Huy Hào";
 const link = document.querySelector("link[rel*='icon']") as HTMLLinkElement || document.createElement('link');
 link.type = 'image/jpeg';
 link.rel = 'icon';
-link.href = '/img/logo.jpg';
+link.href = '/img/logo.jpg?v=' + new Date().getTime();
 document.getElementsByTagName('head')[0].appendChild(link);
 
 createRoot(document.getElementById('root')!).render(
